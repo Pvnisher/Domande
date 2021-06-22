81620eee5e61784f8fdad3e2d862c532176a70db
damned <62070968+damned-me@users.noreply.github.com>
2021-06-22 02:46:53 +0200

    Prepare to merge

--- main.cpp
+++ main.cpp
@@ -111,7 +111,6 @@ int main(int argc, char **argv) {
 void write_headers(const string& output_file, const string& headers_file)
 {
     fstream file;
-<<<<<<< HEAD
     fstream input;
     file.open(output_file, std::ios_base::out);
     if (file.is_open()){
@@ -146,27 +145,4 @@ void write_headers(const string& output_file, const string& headers_file)
         }
         file.close();
     }
-=======
-    file.open(nomefile, ios::out);
-    //inizio e script w3school
-    file << "<html><head><script src='https://www.w3schools.com/lib/w3.js'></script>";
-    //mio script
-    file << "<script>var x;var num;\nfunction cambia(t){\nif(t=='a' && x<document.querySelectorAll('.sdrogo').length){\ndocument.getElementById(x).style=\"display:none;\"\n";
-    file << "if(rand==true)\nx=Math.floor((Math.random() * num) + 1);\nelse\nx++;\nw3.hide('.risposta');\ndocument.getElementById(x).style=\"display:inline-block;\"}\nif(t=='i' && x>1){\ndocument.getElementById(x).style=\"display:none;\"\n";
-    file << "if(rand==true)\nx=Math.floor((Math.random() * num) + 1);\nelse\nx--;\ndocument.getElementById(x).style=\"display:inline-block;\"}}\nfunction start(){\n";
-    file << "x=1;num=document.querySelectorAll('.sdrogo').length;\ndocument.getElementById(1).style=\"display:inline-block;\";\nw3.toggleShow('.risposta');}\n";
-    file << "function toggleRand(){\nif(rand==true){\ndocument.getElementById('rand').value=\"rand disattivo\";\nrand=false;}\n";
-    file << "else{\ndocument.getElementById('rand').value=\"rand attivo\";\nrand=true;}}\n</script>\n";
-    //css e chiudo head
-    file << "<style>.container {height:85%; max-height:85%; display: inline-block; overflow: scroll; width: 100%;}.domanda{font-weight: bold;}.sdrogo{width: 80%; display:none;}</style></head>";
-    //bottoni
-    file << "<body onload='start()'>";
-    file << "<input type=button id='nascondimostra' style='position:fixed; margin-left: 85%' onclick=\"w3.toggleShow('.risposta')\" value='Mostra/nascondi riposte'>";
-    file << "<input type=button id='next' style='position:fixed; margin-left: 85%; margin-top: 10%;' onclick=\"cambia('a')\" value='Prossimo'>";
-    file << "<input type=button id='back' style='position:fixed; margin-left: 85%; margin-top: 12%;' onclick=\"cambia('i')\" value='Indietro'>";
-    file << "<input type=button id='rand' style='position:fixed; margin-left: 85%; margin-top: 5%;' onclick='toggleRand()'  value='rand disattivo'>";
-<<<<<<< HEAD
->>>>>>> e365900e49d49f63151a8cd727cd6744d378e8e5
-=======
->>>>>>> e365900e49d49f63151a8cd727cd6744d378e8e5
 }
